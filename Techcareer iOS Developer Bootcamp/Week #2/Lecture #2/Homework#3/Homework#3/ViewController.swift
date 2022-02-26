900fa1b35cffb115d345c34b32e954a9b56fb1bf
Can Bi <bica@mef.edu.tr>
2022-02-26 16:22:16 +0300

    Homework #3

---
+++ Techcareer iOS Developer Bootcamp/Week #2/Lecture #2/Homework#3/Homework#3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework#3
+//
+//  Created by Can Bi on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
